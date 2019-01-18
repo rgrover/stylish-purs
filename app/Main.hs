d293109387fbdee7e576a6ac0dceb3d0c02b6e86
Rohit Grover <rohit@mailbox.org>
2019-01-19 10:32:04 +1300

    add a formatting quasiquoter

--- app/Main.hs
+++ app/Main.hs
@@ -1,85 +1,20 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import           Language.PureScript.AST.Declarations
-import           Language.PureScript.AST.SourcePos
-import           Language.PureScript.Names
-import           Language.PureScript.Parser.Declarations (parseModuleFromFile,
-                                                          toPositionedError)
-import           Language.PureScript.Types
-
-import           System.IO.UTF8                          (readUTF8FileT)
-
-import           Data.Text                               (intercalate,
-                                                          singleton,
-                                                          unpack)
-
-import qualified Data.Text.Prettyprint.Doc               as P
-import qualified Data.Text.Prettyprint.Doc.Util          as P
-
-import           System.Exit
-
-path = "/home/rohit/swim/Purescript/my-project/src/Main.purs"
+import           Formatter
 
 main :: IO ()
 main = do
-    t <- readUTF8FileT path
-    case parseModuleFromFile id (path, t) of
-        Left e       -> die $ show e
-        --Right (_, m) -> print m >> exitSuccess
-        Right (_, m) -> do
-            let ModuleName moduleNameComponents = getModuleName m
-                fullModuleName =
-                    intercalate
-                        (singleton '.')
-                        (runProperName <$> moduleNameComponents)
-            print fullModuleName
-            P.putDocW 80 $ emitDeclaration $ head $ getModuleDeclarations m
-            exitSuccess
+    let out = [fmt|
+module Test.Main where
 
-emitDeclaration :: Declaration -> P.Doc ann
-emitDeclaration (TypeDeclaration d) =
-    emitTypeDeclIdent (tydeclIdent d) P.<+> P.pretty ("::" :: String) P.<+>
-    emitType (tydeclType d) <>
-    P.line
-emitDeclaration _ = P.pretty ("unhandled declaration type" :: String)
+diagonal :: Number → Number → Number
+diagonal w h = sqrt (w * w + h * h) -- testing comments
 
-emitTypeDeclIdent :: Ident -> P.Doc ann
-emitTypeDeclIdent (Ident t) = P.pretty t
-emitTypeDeclIdent _         = error "unhandled ident"
-
-emitType :: Type -> P.Doc ann
-emitType (TypeConstructor (Qualified Nothing n)) = P.pretty $ runProperName n
-emitType (TypeApp
-            (TypeConstructor
-                (Qualified (Just (ModuleName [ProperName "Prim"]))
-                    (ProperName "Function")))
-            (TypeConstructor
-                (Qualified Nothing (ProperName n)))) =
-                    P.pretty (unpack n) P.<+> "->"
-emitType (TypeApp
-            t1@(TypeApp _ _)
-            (TypeConstructor
-                (Qualified Nothing (ProperName n)))) =
-                    emitType t1 P.<+> P.pretty (unpack n)
-emitType (TypeApp t1@(TypeApp _ _) t2@(TypeApp _ _)) =
-    emitType t1 P.<+> emitType t2
-emitType _ = error "unhandled type"
-
-    {-
-       {- rohit
-           -}
-       module Test.Main where
-
-
-       diagonal :: Number → Number → Number
-       diagonal w h = sqrt (w * w + h * h) -- testing comments
-
-       main :: Effect Unit
-       main = logShow $ diagonal 3.0 4.0
-
-       -}
+main :: Effect Unit
+main = logShow $ diagonal 3.0 4.0
+     |]
+    putStrLn out
 
     {-
 ValueDeclaration
