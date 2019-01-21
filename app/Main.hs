9b6a293dd15b5fdd8e62414502b066de73ae4e59
Rohit Grover <rohit@mailbox.org>
2019-01-21 13:24:28 +1300

    main application reads input from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
-{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import           Quoter
+import           Format.Formatter
+
+import           Language.PureScript
+import           System.IO.UTF8      (readUTF8File)
+
+import           Data.Text.IO        as T (getContents, putStr)
+import           System.Exit
+import qualified System.IO           as IO
 
 main :: IO ()
 main = do
-    let out = [fmt|
-module Test.Main where
-
-diagonal :: Number → Number → Number
-diagonal w h = sqrt (w * w + h * h + 2.0) -- testing comments
-
-main :: Effect Unit
-main = logShow $ diagonal 3.0 4.0
-     |]
-    putStrLn out
+    IO.hSetEncoding IO.stdout IO.utf8
+    IO.hSetEncoding IO.stderr IO.utf8
+    text <- T.getContents
+    case parseModuleFromFile id ("_stdin", text) of
+        Left error         -> print error >> exitFailure
+        Right (_, parsedModule) -> T.putStr $ formatModule parsedModule
--- src/Format/Formatter.hs
+++ src/Format/Formatter.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Format.Formatter (format) where
+module Format.Formatter (format, formatModule) where
 
 import           Language.PureScript.AST.Declarations
 import           Language.PureScript.Names
@@ -8,29 +8,33 @@ import           Language.PureScript.Parser.Declarations (parseModuleFromFile)
 import           Data.Text.Prettyprint.Doc
 import           Data.Text.Prettyprint.Doc.Render.Text
 
-import           Data.Text                               (intercalate,
+import           Format.Declaration
+
+import           Data.Text                               (Text,
+                                                          intercalate,
                                                           pack,
                                                           unpack)
-
-import           Format.Declaration
 
 format :: String -> String
 format s =
   case parseModuleFromFile id ("_quoted", pack s) of
-      Left e       -> "failed to parse input"
-      Right (_, m) ->
-          let header          = prettyModuleHeader m
-              decls           = getModuleDeclarations m
-              declarationDocs = (pretty . ModuleDeclarations) decls
-              doc             = if null decls
-                then header
-                else header <> line <> declarationDocs
-          in unpack . renderStrict . layoutPretty defaultLayoutOptions $ doc
+      Left e                  -> "failed to parse input"
+      Right (_, parsedModule) -> unpack $ formatModule parsedModule
+
+formatModule :: Module -> Text
+formatModule parsedModule =
+    let header          = prettyModuleHeader parsedModule
+        decls           = getModuleDeclarations parsedModule
+        declarationDocs = (pretty . ModuleDeclarations) decls
+        doc             = if null decls
+            then header
+            else header <> line <> declarationDocs
+    in renderStrict . layoutPretty defaultLayoutOptions $ doc
 
 prettyModuleHeader :: Module -> Doc ann
-prettyModuleHeader m =
+prettyModuleHeader parsedModule =
     "module" <+> prettyName <+> "where" <> line
   where
-    ModuleName nameComponents = getModuleName m
+    ModuleName nameComponents = getModuleName parsedModule
     prettyName = pretty $
         intercalate "." (runProperName <$> nameComponents)
