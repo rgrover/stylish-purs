5ad2f7820e506e82af03ce0be859f4fbadab5f24
Rohit Grover <rohit@mailbox.org>
2019-01-26 10:51:08 +1300

    read input from filename or stdin

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,9 @@ import           Format.Formatter
 import           Language.PureScript
 import           System.IO.UTF8      (readUTF8File)
 
-import           Data.Text.IO        as T (getContents, putStr)
+import           Data.Text.IO        as T (getContents, putStr,
+                                           readFile)
+import           System.Environment
 import           System.Exit
 import qualified System.IO           as IO
 
@@ -13,7 +15,10 @@ main :: IO ()
 main = do
     IO.hSetEncoding IO.stdout IO.utf8
     IO.hSetEncoding IO.stderr IO.utf8
-    text <- T.getContents
+    args <- getArgs
+    text <- if null args
+                then T.getContents
+                else T.readFile $ head args
     case parseModuleFromFile id ("_stdin", text) of
         Left error              -> print error >> exitFailure
         Right (_, parsedModule) -> T.putStr $ formatModule parsedModule
